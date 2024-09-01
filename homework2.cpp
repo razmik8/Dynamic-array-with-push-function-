7370f6f885ecae77b3d97fcbe67f15cf9545081f
razmik8 <razmik2002ghazaryan@mail.ru>
2024-09-01 14:55:00 +0400

    Update homework01,09.cpp

--- homework2.cpp
+++ homework2.cpp
@@ -1,33 +1,65 @@
 #include <iostream>
-
-class myclass{
+/*class Intarr
+{
+    int * arr;
+}*/
+class myclass
+{
 	private:
 		int*arr;
 		int size;
 		int capacity;
 	public:
-		myclass(){
+	
+		myclass()
+		{
 			arr = new int[2];
 			size = 0;
 			capacity = 2;
 		}
-		myclass(int value,int Capacity){
-			for(int i = 0;i < capacity;i++){
+		
+		myclass(int value,int Capacity)
+		{
+		    arr = new int[Capacity];
+		    capacity = Capacity;
+		    size = 0;
+			for(int i = 0;i < capacity;i++)
+			{
 				*(arr + i) = value + i;
+				size++;
 			}
-			capacity = Capacity;
 		}
-		myclass(const myclass& obj){
-			arr = obj.arr;
+		
+		myclass(const myclass& obj)
+		{
+			arr = new int[obj.capacity];
 			size = obj.size;
 			capacity = obj.capacity;
+			for(int i = 0; i < size; i++)
+			{
+			    arr[i] = obj.arr[i];
+			}
 		}
-		void push(int val){
-			if(size == capacity){
+		
+		myclass(myclass&& obj)
+		{
+		    arr = obj.arr;
+		    size = obj.size;
+		    capacity = obj.capacity;
+		    obj.arr = nullptr;
+		    obj.size = 0;
+		    obj.capacity = 0;
+		}
+		
+		void push(int val)
+		{
+			if(size == capacity)
+			{
 				int*tmp = nullptr;
 				capacity *= 2;
 				tmp = new int[capacity];
-				for(int i = 0;i < size;i++){
+				for(int i = 0;i < size;i++)
+				{
 					*(tmp + i) = *(arr + i);
 				}
 				delete[] arr;
@@ -37,25 +69,65 @@ class myclass{
 			*(arr + size) = val;
 			size++;
 		}
+		
 		void print(){
-			for(int i = 0;i < size;i++){
+			for(int i = 0;i < size;i++)
+			{
 				std::cout << *(arr + i);
 			}
 		}
-		~myclass(){
-			if(arr != nullptr){
+		
+		~myclass()
+		{
+			if(arr != nullptr)
+			{
 				delete[] arr;
 			}
 			arr = nullptr;
+		}
+		myclass& operator=(const myclass& obj)
+		{
+		    if(&obj == this)
+		    {
+		        return *this;
+		    }
+		    arr = new int[obj.capacity];
+		    size = obj.size;
+		    capacity = obj.capacity;
+		    for(int i = 0; i < capacity; i++)
+		    {
+		        arr[i] = obj.arr[i];
+		    }
+		    return *this;
+		}
+		
+		myclass& operator=(myclass&& obj)
+		{
+		    if(this == &obj)
+		    {
+		        return *this;
+		    }
+		    capacity = obj.capacity;
+		    size = obj.size;
+		    delete[] arr;
+		    arr = obj.arr;
+		    obj.arr = nullptr;
+		    obj.size = 0;
+		    obj.capacity = 0;
+		    return *this;
 		}
 };
 
 int main(){
 	myclass ara;
-	ara.push(4);
-	ara.push(3);
+	myclass aru;
+	
 	ara.push(2);
+	ara.push(3);
+	ara.push(8);
 	ara.push(1);
+	aru = std::move(ara);
+	aru.print();
 	ara.print();
 	return 0;
 }
